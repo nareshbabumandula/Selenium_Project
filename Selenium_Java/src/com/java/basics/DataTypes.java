6b00f4a6a73f375faa44640bae603dac6d6339c1
Mandula Naresh Babu <naresh223@gmail.com>
2021-10-11 10:27:14 +0530

    First Commit
    Selenium Java Project

---
+++ Selenium_Java/src/com/java/basics/DataTypes.java
@@ -0,0 +1,30 @@
+package com.java.basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Java is case sensitive language
+		// Primitive data types
+		// Always use Ctrl+Spacebar for completing the syntax
+		// Run the Java Application - Click on Run button in toolbar/Right on Source Code->Run As->Java Application/Ctrl+F11
+		byte b=112;
+		short s=2145;
+		int i=434636;
+		long l=366784;
+		char c='t';
+		float f=13.6f;
+		double d=436.756;
+		boolean bflag=true;
+		
+		System.out.println("Byte value of b is : " +b);
+		System.out.println("Short value of s is : " +s);
+		System.out.println("Integer value of i is : "+i);
+		System.out.println("Long value of l is : "+l);
+		System.out.println("Char value of c is : "+c);
+		System.out.println("Float value of f is : "+f);
+		System.out.println("Double value of d is : "+d);
+		System.out.println("Boolean value of bflag is : "+bflag);
+		
+	}
+
+}
