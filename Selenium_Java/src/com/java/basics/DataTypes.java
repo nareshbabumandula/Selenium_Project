45482fa0a86b9c35a55a589fc8a3fff6b93c3c8b
Mandula Naresh Babu <naresh223@gmail.com>
2021-10-12 10:09:30 +0530

    12-Oct-2021:Naresh: Type Casting: Widening and Narrowing

--- Selenium_Java/src/com/java/basics/DataTypes.java
+++ Selenium_Java/src/com/java/basics/DataTypes.java
@@ -11,9 +11,9 @@ public class DataTypes {
 		short s=2145;
 		int i=434636;
 		long l=366784;
-		char c='t';
 		float f=13.6f;
 		double d=436.756;
+		char c='t';
 		boolean bflag=true;
 		
 		System.out.println("Byte value of b is : " +b);
@@ -24,6 +24,25 @@ public class DataTypes {
 		System.out.println("Float value of f is : "+f);
 		System.out.println("Double value of d is : "+d);
 		System.out.println("Boolean value of bflag is : "+bflag);
+		
+		//Type casting: Converting is nothing converting one type of primitive data into another primitive type
+		// Widening/Automation conversion: Converting the smaller datatype to the bigger datatype
+		//e.g. byte->short->int->long->float->double
+		byte b1=125;
+		short s1=b1; //Widening/Automation conversion
+		System.out.println(b1 + " " + s1);
+		
+		// Narrowing/Explicit casting: Converting the bigger datatype to the smaller datatype: double->float->long->int->short->byte
+		int i1=129;
+		//byte b2=i1; //Compilation error - Unresolved compilation problem
+		byte b2=(byte)i1;
+		System.out.println(i1 + " " +b2);
+		
+		//Wrapper classes - Pre-defined java classes for all the primitive datatypes exists
+		System.out.println("Min value of byte is : "+ Byte.MIN_VALUE);
+		System.out.println("Max value of byte is : "+ Byte.MAX_VALUE);
+	
+
 	}
 
 }
