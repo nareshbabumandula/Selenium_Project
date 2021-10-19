02172f25c559088c45c08c78cbbcb3e108fda583
Madhu297 <madhuarya.sg@gmail.com>
2021-10-19 12:24:14 +0800

    19-Oct-2021: Madhu: Added Java_Practice.java file

---
+++ Selenium_Java/src/com/assignment/Java_Practice.java
@@ -0,0 +1,45 @@
+package com.assignment;
+
+public class Java_Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+	    boolean flag = true;
+	    System.out.println(flag); 
+	    
+        float f1 = 1.5f;
+        float f2 = 2.5f;
+        
+        float p = f1*f2;
+        
+        System.out.println("The product is: " +p);
+        
+        int first = 10;
+        int second = 20;
+        
+        System.out.println(first + " " +second);
+        
+        int sum = first + second;
+        
+        System.out.println("The sum is: "+sum);
+        
+        int x = 44;
+        int y = 129;
+        int z = x/y;
+        
+        System.out.println("Division: " +z);
+        
+        double num1 = 10;
+        double num2 = 15;
+        
+        System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
+        
+      
+       
+	}
+
+}
