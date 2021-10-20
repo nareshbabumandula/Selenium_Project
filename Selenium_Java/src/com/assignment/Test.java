d9d69b1bc37bed04f05bfaa97e66427cdff6a337
Mandula Naresh Babu <naresh223@gmail.com>
2021-10-20 09:20:23 +0530

    Updated Print statement in Test class

--- Selenium_Java/src/com/assignment/Test.java
+++ Selenium_Java/src/com/assignment/Test.java
@@ -3,8 +3,7 @@ package com.assignment;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test Class");
-
+		System.out.println("Test Class for practice");
 	}
 
 }
